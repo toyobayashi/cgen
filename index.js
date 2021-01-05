76d29da0eb09774c1ebbe3bc171c9407a4579a81
toyobayashi <lifenglin314@outlook.com>
2021-01-05 15:56:44 +0800

    CMP0068

--- index.js
+++ index.js
@@ -157,10 +157,13 @@ function generateCMakeLists (config, configPath, options, isEmscripten, parentPa
   const isMain = !parentPath
 
   cmklists.writeHeadLine(`# ${JSON.stringify(options)}`)
-  cmklists.writeHeadLine(`cmake_minimum_required(VERSION ${config.minimumVersion || '3.7'})`)
+  cmklists.writeHeadLine(`cmake_minimum_required(VERSION ${config.minimumVersion || '3.9'})`)
   
   cmklists.writeHeadLine(`if(\${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.15.0")`)
   cmklists.writeHeadLine(`  cmake_policy(SET CMP0091 NEW)`)
+  cmklists.writeHeadLine(`endif()`)
+  cmklists.writeHeadLine(`if(APPLE)`)
+  cmklists.writeHeadLine(`  cmake_policy(SET CMP0068 NEW)`)
   cmklists.writeHeadLine(`endif()`)
   cmklists.writeHeadLine(`project(${config.project})`)
 
@@ -269,7 +272,7 @@ endif()`)
         target.linkOptions = Array.from(new Set([...(target.linkOptions || []), ...([
           '-undefined dynamic_lookup'
         ])]))
-        cmklists.writeLine(`set_target_properties(${target.name} PROPERTIES BUILD_WITH_INSTALL_RPATH 1 INSTALL_NAME_DIR "@rpath")`)
+        cmklists.writeLine(`set_target_properties(${target.name} PROPERTIES BUILD_WITH_INSTALL_NAME_DIR 1 INSTALL_NAME_DIR "@rpath")`)
       }
       if (process.platform === 'win32' && !isEmscripten) {
         target.staticVCRuntime = typeof target.staticVCRuntime === 'boolean' ? target.staticVCRuntime : true
